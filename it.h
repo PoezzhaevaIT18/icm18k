b81a5c6b934b15bbc0a536301b6b9de0bdb29aa5
PoezzhaevaIT18 <karinapoezzhaeva@gmail.com>
2025-01-10 18:42:22 +0500

    Add files via upload

---
+++ it.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+#include <string>
+
+std::string step(int n);
+bool f(const std::string& input);
+
+#endif // MATH_UTILS_H
---
+++ it18.icm.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "it.h" // подключаем заголовочный файл
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string input;
+    int n;
+
+    cout << "Введите n (больше 40): ";
+
+    while (true) { // бесконечный цикл для обработки пользовательского ввода
+        getline(cin, input); // считываем всю строку
+
+        if (!f(input)) { // проверяем корректность ввода
+            cout << "Ввод некорректен , введите число больше 40: ";
+            continue;
+        }
+
+        n = stoi(input); // преобразуем строку в целое число
+
+        if (n <= 40) { // проверяем, что n больше 40
+            cout << " Ввод некорректен , введите число больше 40: ";
+            continue;
+        }
+
+        break; // корректный ввод, выходим из цикла
+    }
+
+    string r = step(n);
+    cout << "Результат 2^" << n << " - 1 = " << r << endl;
+
+    return 0;
+}
